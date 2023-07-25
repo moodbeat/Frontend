294d891bc63ecfde33078b27f0235ba1b728aa97
Kirill Kurentsov <immortalsteel@gmail.com>
2023-07-25 05:33:45 +0300

    fix: delete discuss button on Tests page

--- src/components/TestResultPopup/TestResultPopup.tsx
+++ src/components/TestResultPopup/TestResultPopup.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
 import styles from "./testResultPopup.module.css";
-import ReactMarkdown from 'react-markdown';
-
+import ReactMarkdown from "react-markdown";
 import { WarningWithBall } from "../WarningWithBall/WarningWithBall";
 import { WarningWithLine } from "../WarningWithLine/WarningWithLine";
 import cn from "classnames";
-
 import { ExpressDiagnoseResponse } from "@/types";
 
 interface TestResultPopup {
@@ -15,9 +13,15 @@ interface TestResultPopup {
   isTestsReulstLocated: boolean;
 }
 
-export const TestResultPopup: React.FC<TestResultPopup> = ({resultOfPsychoTest, isVisible, onClose, isTestsReulstLocated}) => {
-
-  const [isResultsPopapVisible, setResultsPopapVisible] = useState<boolean | undefined>(false);
+export const TestResultPopup: React.FC<TestResultPopup> = ({
+  resultOfPsychoTest,
+  isVisible,
+  onClose,
+  isTestsReulstLocated,
+}) => {
+  const [isResultsPopapVisible, setResultsPopapVisible] = useState<
+    boolean | undefined
+  >(false);
 
   const closePopupClassname = cn(styles.resultsPopup, {
     [styles.hidden]: isResultsPopapVisible === false,
@@ -29,11 +33,7 @@ export const TestResultPopup: React.FC<TestResultPopup> = ({resultOfPsychoTest, 
     if (onClose) onClose();
   }
 
-  function handleCallChief() {
-    alert("Отправлена заявка на разговор с руководителем");
-  }
-
-  useEffect(() => (setResultsPopapVisible(isVisible)), [isVisible]);
+  useEffect(() => setResultsPopapVisible(isVisible), [isVisible]);
 
   return (
     <section className={closePopupClassname}>
@@ -46,57 +46,71 @@ export const TestResultPopup: React.FC<TestResultPopup> = ({resultOfPsychoTest, 
       </div>
       <p>{resultOfPsychoTest?.mental_state.message}</p>
 
-      {(resultOfPsychoTest?.survey.type === 'yn') ?
+      {resultOfPsychoTest?.survey.type === "yn" ? (
         <>
-          {resultOfPsychoTest?.survey.text.length > 0 ? <h2 className={styles.subtitle}>Рекомендации</h2> : null}
+          {resultOfPsychoTest?.survey.text.length > 0 ? (
+            <h2 className={styles.subtitle}>Рекомендации</h2>
+          ) : null}
           <ReactMarkdown>{resultOfPsychoTest?.survey.text}</ReactMarkdown>
-          <div className={styles.buttonContainer}>
-            {(resultOfPsychoTest?.mental_state.level !== 1) && <button type='button' onClick={handleCallChief} className={styles.button}>Обсудить с руководителем</button>}
-          </div>
         </>
-          :
+      ) : (
         <>
           <h2 className={styles.subtitle}>Расшифровка</h2>
 
-          {resultOfPsychoTest?.summary?.graphs?.map(graph => {
-
+          {resultOfPsychoTest?.summary?.graphs?.map((graph) => {
             switch (graph.size) {
-                // case 'big':
-                // return <div className={styles.summaryGraph} key={graph.title.slice(-8)}>
-                //   <h3 className={styles.conditionText}>{graph.title}: {parseInt((graph.value * 100).toString(), 10)} %</h3>
-                //   <WarningWithLine resultOfPsychoTest={resultOfPsychoTest} graph={graph}/>
-                // </div>;
-                // break;
-                case 'big':
-                  return null;
-                  break;
+              // case 'big':
+              // return <div className={styles.summaryGraph} key={graph.title.slice(-8)}>
+              //   <h3 className={styles.conditionText}>{graph.title}: {parseInt((graph.value * 100).toString(), 10)} %</h3>
+              //   <WarningWithLine resultOfPsychoTest={resultOfPsychoTest} graph={graph}/>
+              // </div>;
+              // break;
+              case "big":
+                return null;
 
-                case 'medium':
-                  return <div className={styles.summaryGraph} key={graph.title.slice(-8)}>
-                  <h4 className={styles.conditionSubtitle}>{graph.title}</h4>
-                  <WarningWithLine resultOfPsychoTest={resultOfPsychoTest} graph={graph}/>
-                </div>
-                break;
+              case "medium":
+                return (
+                  <div
+                    className={styles.summaryGraph}
+                    key={graph.title.slice(-8)}
+                  >
+                    <h4 className={styles.conditionSubtitle}>{graph.title}</h4>
+                    <WarningWithLine
+                      resultOfPsychoTest={resultOfPsychoTest}
+                      graph={graph}
+                    />
+                  </div>
+                );
 
-                case 'small':
-                  return <div className={styles.summaryGraph} key={graph.title.slice(-8)}>
-                  <h5 className={styles.conditionSmallSubtitle}>{graph.title}</h5>
-                  <WarningWithLine resultOfPsychoTest={resultOfPsychoTest} graph={graph}/>
-                </div>
-                break;
-              }
-            })
-          }
+              case "small":
+                return (
+                  <div
+                    className={styles.summaryGraph}
+                    key={graph.title.slice(-8)}
+                  >
+                    <h5 className={styles.conditionSmallSubtitle}>
+                      {graph.title}
+                    </h5>
+                    <WarningWithLine
+                      resultOfPsychoTest={resultOfPsychoTest}
+                      graph={graph}
+                    />
+                  </div>
+                );
+            }
+          })}
 
-          <ReactMarkdown children={(resultOfPsychoTest?.survey.text !== undefined) ? resultOfPsychoTest?.survey.text : ''}/>
-
-          <div className={styles.buttonContainer}>
-            {(resultOfPsychoTest?.mental_state.level !== 1) && <button type='button' onClick={handleCallChief} className={styles.button}>Обсудить с руководителем</button>}
-          </div>
+          <ReactMarkdown
+            children={
+              resultOfPsychoTest?.survey.text !== undefined
+                ? resultOfPsychoTest?.survey.text
+                : ""
+            }
+          />
         </>
-      }
+      )}
 
-      <button type='button' onClick={handleClose} className={styles.closeBtn}/>
+      <button type="button" onClick={handleClose} className={styles.closeBtn} />
     </section>
   );
 };
