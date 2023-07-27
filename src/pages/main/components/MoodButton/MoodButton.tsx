946ebef018b94dcb2496d8eac65b3fc721d268b7
Anastasia Sergeeva <lirebook@yandex.ru>
2023-07-27 15:52:18 +0300

    styles: change hover and active styles form mood buttons

--- src/pages/main/components/MoodButton/MoodButton.tsx
+++ src/pages/main/components/MoodButton/MoodButton.tsx
@@ -1,4 +1,5 @@
-import styles from "./MoodButton.module.css";
+import React, { useState } from "react";
+import styles from "./MoodButton.module.scss";
 import cn from "classnames";
 import { useAppDispatch, useAppSelector } from "@/store/hooks.ts";
 import { setSuccessMessage } from "@/store/reducers/alertSuccess/alertSuccessReducer.ts";
@@ -11,54 +12,54 @@ interface MoodsProps {
 }
 
 export const MoodButton: React.FC<MoodsProps> = ({ mood }) => {
-
   const dispatch = useAppDispatch();
   const buttonCondition = useAppSelector(selectButtonConditions);
 
+  const [isHovered, setIsHovered] = useState(false);
+
   let caption: string;
   let imgUrl: string;
+  let imgUrlHovered: string;
   let colorClass: string;
   let condition: number;
 
   switch (mood) {
     case "bad":
       caption = "Плохо";
-      imgUrl = "/worried.svg";
       colorClass = styles.moodButtonRed;
+      imgUrl = '/worried.svg';
+      imgUrlHovered = '/worried-hover.svg';
       condition = 1;
       break;
     case "so-so":
       caption = "Так себе";
       imgUrl = "/confused.svg";
+      imgUrlHovered = '/confused-hover.svg';
       colorClass = styles.moodButtonOrange;
       condition = 2;
       break;
     case "normal":
       caption = "В норме";
       imgUrl = "/expressionless.svg";
+      imgUrlHovered = '/expressionless-hover.svg';
       colorClass = styles.moodButtonYellow;
       condition = 3;
       break;
     case "good":
       caption = "Хорошо";
       imgUrl = "/slightly-smile.svg";
+      imgUrlHovered = '/slightly-smile-hover.svg';
       colorClass = styles.moodButtonGreen;
       condition = 4;
       break;
     case "perfect":
       caption = "Отлично";
       imgUrl = "/simple-smile.svg";
+      imgUrlHovered = '/simple-smile-hover.svg';
       colorClass = styles.moodButtonSupergreen;
       condition = 5;
       break;
-
-    // default:
-    //   caption = "Отлично";
-    //   imgUrl = "/simple-smile.svg";
-    //   colorClass = styles.moodButtonSupergreen;
-    //   condition = 5;
   }
-
 
   function handleSendMood(e: React.MouseEvent<HTMLButtonElement>) {
     if (buttonCondition) {
@@ -78,9 +79,16 @@ export const MoodButton: React.FC<MoodsProps> = ({ mood }) => {
   }
 
   return (
-    <button onClick={(e) => handleSendMood(e)} value={condition} className={cn(styles.container, colorClass)}>
-      <span>{caption}</span>
-      <img src={imgUrl} alt="icon" />
+    <button
+      onClick={(e) => handleSendMood(e)}
+      value={condition}
+      className={cn(styles.container, colorClass)}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {caption}
+      <img src={isHovered ? imgUrlHovered : imgUrl} alt="icon" />
     </button>
   );
 };
+
