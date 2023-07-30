bef64b2a1551f9379d9a1aa2f87d8dfd8f5d9ff1
Alix2019 <alix1982@yandex.ru>
2023-07-30 22:06:27 +0300

    remove_fix_Useful

--- src/pages/useful/Useful.tsx
+++ src/pages/useful/Useful.tsx
@@ -1,6 +1,6 @@
 import styles from "./useful.module.scss";
 import {Navbar} from "@/components/Navbar/Navbar";
-import { ContainerContent } from "@/shared/components/ContainerContent/ContainerContent";
+// import { ContainerContent } from "@/shared/components/ContainerContent/ContainerContent";
 import {useOnlineCheck} from "@/shared/hooks/useOnlineCheck";
 import SearchUseful from "@/components/SearchUseful/SearchUseful";
 import TagsList from "@/components/TagsList/TagsList";
@@ -127,8 +127,8 @@ export const Useful = () => {
     <div className="page-container">
       <Navbar/>
       {isOnline ?
-        <ContainerContent>
-        {/* <div className={styles.container}> */}
+        // <ContainerContent>
+        <div className={styles.container}>
           <div className={styles.useful}>
             <h2 className={styles.title}>Полезные статьи и видео</h2>
             <SearchUseful onSearch={handleSearchCards}/>
@@ -142,8 +142,8 @@ export const Useful = () => {
               </>}
               <ButtonTelegramm />
           </div>
-        {/* </div> */}
-        </ContainerContent>
+        </div>
+        // </ContainerContent>
         : <BadInternetConnection/>}
     </div>
   );
