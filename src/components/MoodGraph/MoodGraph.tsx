f9c1b37a1fe5a784c9990b4358c77c1e700aced6
RomanRiyanov <yezgo123@gmail.com>
2023-07-31 23:57:20 +0600

    fix: fix MoodGraph diagram on main

--- src/components/MoodGraph/MoodGraph.tsx
+++ src/components/MoodGraph/MoodGraph.tsx
@@ -1,4 +1,4 @@
-import { ResponsiveLine } from "@nivo/line";
+import { ResponsiveBar } from "@nivo/bar";
 import { useState, useEffect } from "react";
 import { selectConditions, selectButtonConditions } from "@/store/reducers/conditionsBurnout/conditionsBurnoutReducer.ts";
 import { useAppSelector } from "@/store/hooks.ts";
@@ -8,14 +8,15 @@ import simpleSmileIcon from '../../../public/simple-smile.svg';
 import slightlySmileIcon from '../../../public/slightly-smile.svg';
 import expressionlessIcon from '../../../public/expressionless.svg';
 import worriedIcon from '../../../public/worried.svg';
-
 import styles from "./MoodGraph.module.css";
 import {UserConditionRecieved} from "@/types.ts";
 import {useLocation} from "react-router";
 
+const shamefulMonthsArray = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+
 interface renderData {
-  x: number,
-  y: number
+  x: number | string,
+  y: number | string
 }
 
 interface Props {
@@ -28,13 +29,12 @@ export const MoodGraph = ({conditionsData}: Props) => {
   const [monthVisible, setMonthVisible] = useState <number>(0);
   const [currentYear, setCurrentYear] = useState<number>(2023);
   const [numberOfDays, setNumberOfDays] = useState<number>(30);
-  const [calendar, setCalendar] = useState<number[]>([]);
 
   const conditionsRecieved = useAppSelector(selectConditions);
   const buttonConditions = useAppSelector(selectButtonConditions);
   const { pathname } = useLocation();
 
-  function generateData () {
+  function generateData (numberOfDays: number) {
 
     //conditionsRecieved currentYear monthVisible numberOfDays входящие переменные, функцию можно вынести
     const renderData: renderData[] = [];
@@ -70,9 +70,7 @@ export const MoodGraph = ({conditionsData}: Props) => {
   }
 
   function getMonthName (monthNumber: number) {
-    const date = new Date();
-    date.setMonth(monthNumber);
-    return date.toLocaleString('ru', { month: 'long' });
+    return shamefulMonthsArray[monthNumber];
   }
 
   function decrease () {
@@ -98,34 +96,25 @@ export const MoodGraph = ({conditionsData}: Props) => {
       setConditionValues(conditionsRecieved.slice(0));
     }
 
-    setData(generateData());
+    setData(generateData(numberOfDays));
   }, [buttonConditions, pathname, conditionsData, conditionsRecieved]);
 
   useEffect(() => {
     const newNumberOfDays = getNumberOfVisibleMonth(currentYear, monthVisible + 1);
     setNumberOfDays(newNumberOfDays);
-    setData(generateData());
+    setData(generateData(newNumberOfDays));
   }, [monthVisible, currentYear]);
-
-  useEffect(() => {
-    const newCalendar = [];
-    for (let i = 1; i <= numberOfDays; i++) {
-      newCalendar.push(i);
-    }
-    setCalendar(newCalendar);
-  }, [numberOfDays]);
 
   useEffect(() => {
     const date = new Date();
     const month = date.getMonth();
     const year = date.getFullYear();
+    const numberOfDays = getNumberOfVisibleMonth(year, month + 1);
     setMonthVisible(month);
     setCurrentYear(year);
-    setNumberOfDays(getNumberOfVisibleMonth(year, month + 1))
-    setData(generateData());
+    setNumberOfDays(numberOfDays)
+    setData(generateData(numberOfDays));
   }, []);
-
-  const data3 = [{ id: "mood", data: data }];
 
   return (
     <div className={styles.container}>
@@ -151,27 +140,29 @@ export const MoodGraph = ({conditionsData}: Props) => {
         <img src={worriedIcon} alt="смайлик плохого настроения"/>
       </div>
 
-      <ResponsiveLine
-        data={data3}
-        margin={{ top: 50, right: 0, bottom: 40, left: 33 }}
+      <ResponsiveBar
+        data={data}
+        margin={{ top: 50, right: 0, bottom: 40, left: 30 }}
+        padding={0.35}
         // xScale={{ type: "point" }}
-        curve="basis"
+        keys={["y"]}
+        indexBy={"x"}
+        // curve="basis"
         lineWidth={1.5}
+        borderRadius={5}
         enablePoints={false}
         enableGridX={false}
         enableGridY={false}
-        enableArea={true}
+        enableLabel={false}
         areaOpacity={0.1}
-        colors={'#8A32E0'}
         animate={true}
         defs={[
           {
             id: 'gradient',
             type: 'linearGradient',
             colors: [
-                { offset: 0, color: '#4c239d' },
-                { offset: 50, color: '#4c239d' },
-                { offset: 100, color: 'white' },
+                { offset: 0, color: '#BA99FF' },
+                { offset: 100, color: '#DCCDFC' },
             ],
           },
         ]}
@@ -179,15 +170,10 @@ export const MoodGraph = ({conditionsData}: Props) => {
           { match: '*', id: 'gradient' },
         ]}
         axisLeft={null}
-        axisBottom={null}
+        axisBottom={{
+          tickSize: 0,
+         }}
       />
-
-      <div className={styles.xAxis}>
-        {calendar && calendar.map(day => (
-          <p key={day * 2} className={styles.xAxisText}>{day}</p>
-        ))}
-      </div>
-
     </div>
   );
 };
