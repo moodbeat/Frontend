64017595c3eec026d7750f2a45f66af627424291
ArtemKhudiakov <artemka86ber@gmail.com>
2023-08-01 05:33:00 -0300

    fix: data moodgraph

--- src/components/MoodGraph/MoodGraph.tsx
+++ src/components/MoodGraph/MoodGraph.tsx
@@ -1,8 +1,9 @@
-import { ResponsiveBar } from "@nivo/bar";
-import { useState, useEffect } from "react";
-import { selectConditions, selectButtonConditions } from "@/store/reducers/conditionsBurnout/conditionsBurnoutReducer.ts";
-import { useAppSelector } from "@/store/hooks.ts";
-import { arrowLeft, arrowRight } from "@/assets";
+import {BarDatum, ResponsiveBar} from "@nivo/bar";
+import {useEffect, useState} from "react";
+import {useLocation} from "react-router";
+import {selectButtonConditions, selectConditions} from "@/store/reducers/conditionsBurnout/conditionsBurnoutReducer.ts";
+import {useAppSelector} from "@/store/hooks.ts";
+import {arrowLeft, arrowRight} from "@/assets";
 import confusedIcon from '../../../public/confused.svg';
 import simpleSmileIcon from '../../../public/simple-smile.svg';
 import slightlySmileIcon from '../../../public/slightly-smile.svg';
@@ -10,83 +11,79 @@ import expressionlessIcon from '../../../public/expressionless.svg';
 import worriedIcon from '../../../public/worried.svg';
 import styles from "./MoodGraph.module.css";
 import {UserConditionRecieved} from "@/types.ts";
-import {useLocation} from "react-router";
 
-const shamefulMonthsArray = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
-
-interface renderData {
-  x: number | string,
-  y: number | string
-}
+const shamefulMonthsArray = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
 
 interface Props {
   conditionsData?: UserConditionRecieved[];
 }
 
-export const MoodGraph = ({conditionsData}: Props) => {
-  const [data, setData] = useState <renderData[]>([{ x: 0, y: 0 }]);
+export const MoodGraph = ({conditionsData}: Props): JSX.Element => {
+  const [data, setData] = useState<BarDatum[]>([{x: 0, y: 0}]);
   const [conditionValues, setConditionValues] = useState<UserConditionRecieved[]>([]);
-  const [monthVisible, setMonthVisible] = useState <number>(0);
+  const [monthVisible, setMonthVisible] = useState<number>(0);
   const [currentYear, setCurrentYear] = useState<number>(2023);
   const [numberOfDays, setNumberOfDays] = useState<number>(30);
 
   const conditionsRecieved = useAppSelector(selectConditions);
   const buttonConditions = useAppSelector(selectButtonConditions);
-  const { pathname } = useLocation();
+  const {pathname} = useLocation();
 
-  function generateData (numberOfDays: number) {
-
-    //conditionsRecieved currentYear monthVisible numberOfDays входящие переменные, функцию можно вынести
-    const renderData: renderData[] = [];
+  function generateData(numberOfDays: number): BarDatum[] {
+    const renderData: BarDatum[] = [];
 
     const yearFilteredArray = conditionValues?.filter(item => {
-     const conditionYear = new Date(item.date).getFullYear();
-     return conditionYear === currentYear;
-    })
+      const conditionYear = new Date(item.date).getFullYear();
+      return conditionYear === currentYear;
+    });
 
     const monthFilteredArray = yearFilteredArray?.filter(item => {
       const conditionMonth = new Date(item.date).getMonth();
       return conditionMonth === monthVisible;
-    })
+    });
 
-      for (let i = 1; i <= numberOfDays; i++) {
-        renderData.push({
-          x: i,
-          y: 0,
-        })
-      }
+    for (let i = 1; i <= numberOfDays; i++) {
+      renderData.push({
+        x: i.toString(),
+        y: 0,
+      });
+    }
 
     if (monthFilteredArray) {
-
-     monthFilteredArray?.reverse().forEach((item) => {
+      monthFilteredArray?.reverse().forEach((item) => {
         const day = new Date(item.date).getUTCDate();
 
-        //если на эту дату есть оценка настроения - подменяем её в результирующем массиве
-        if (renderData[day - 1].x === day) renderData[day - 1].y = item.mood;
-      })
+        if (renderData[day - 1]?.x === day.toString()) {
+          renderData[day - 1].y = item.mood;
+        }
+      });
     }
 
     return renderData;
   }
 
-  function getMonthName (monthNumber: number) {
+  function getMonthName(monthNumber: number): string {
     return shamefulMonthsArray[monthNumber];
   }
 
-  function decrease () {
+  function decrease(): void {
     if (monthVisible - 1 >= 0) {
-      setMonthVisible(prevstate => prevstate - 1)
-    } else if (monthVisible - 1 < 0) setMonthVisible(11)
+      setMonthVisible(prevstate => prevstate - 1);
+    } else if (monthVisible - 1 < 0) {
+      setMonthVisible(11);
+    }
   }
 
-  function increase () {
+  function increase(): void {
     if (monthVisible + 1 <= 11) {
-      setMonthVisible(prevstate => prevstate + 1)
-    } else if (monthVisible + 1 > 11) setMonthVisible(0)
+      setMonthVisible(prevstate => prevstate + 1);
+    } else if (monthVisible + 1 > 11) {
+      setMonthVisible(0);
+    }
   }
 
-  function getNumberOfVisibleMonth (year: number, month: number): number {
-    return new Date(year, month, 0).getDate()
+  function getNumberOfVisibleMonth(year: number, month: number): number {
+    return new Date(year, month, 0).getDate();
   }
 
   useEffect(() => {
@@ -112,10 +109,9 @@ export const MoodGraph = ({conditionsData}: Props) => {
     const numberOfDays = getNumberOfVisibleMonth(year, month + 1);
     setMonthVisible(month);
     setCurrentYear(year);
-    setNumberOfDays(numberOfDays)
+    setNumberOfDays(numberOfDays);
     setData(generateData(numberOfDays));
   }, []);
-
   return (
     <div className={styles.container}>
       <div className={styles.topContainer}>
@@ -124,7 +120,7 @@ export const MoodGraph = ({conditionsData}: Props) => {
           <button className={styles.month} onClick={decrease}>
             <div className={styles.buttonContainer}>{arrowLeft}{getMonthName(monthVisible - 1)}</div>
           </button>
-            <strong className={styles.monthVisible}>{getMonthName(monthVisible).toUpperCase()}</strong>
+          <strong className={styles.monthVisible}>{getMonthName(monthVisible).toUpperCase()}</strong>
           <button className={styles.month} onClick={increase}>
             <div className={styles.buttonContainer}>{getMonthName(monthVisible + 1)}{arrowRight}</div>
           </button>
@@ -142,37 +138,41 @@ export const MoodGraph = ({conditionsData}: Props) => {
 
       <ResponsiveBar
         data={data}
-        margin={{ top: 50, right: 0, bottom: 40, left: 30 }}
+        margin={{top: 50, right: 0, bottom: 40, left: 30}}
         padding={0.35}
-        // xScale={{ type: "point" }}
         keys={["y"]}
         indexBy={"x"}
-        // curve="basis"
-        lineWidth={1.5}
         borderRadius={5}
-        enablePoints={false}
         enableGridX={false}
         enableGridY={false}
-        enableLabel={false}
-        areaOpacity={0.1}
+        enableLabel={false} // Set this to true to enable labels on the bars
         animate={true}
         defs={[
           {
             id: 'gradient',
             type: 'linearGradient',
             colors: [
-                { offset: 0, color: '#BA99FF' },
-                { offset: 100, color: '#DCCDFC' },
+              {offset: 0, color: '#BA99FF'},
+              {offset: 100, color: '#DCCDFC'},
             ],
           },
         ]}
         fill={[
-          { match: '*', id: 'gradient' },
+          {match: '*', id: 'gradient'},
         ]}
         axisLeft={null}
         axisBottom={{
           tickSize: 0,
-         }}
+        }}
+        theme={{
+          axis: {
+            ticks: {
+              line: {
+                strokeWidth: 1.5, // Here's the lineWidth customization
+              },
+            },
+          },
+        }}
       />
     </div>
   );
