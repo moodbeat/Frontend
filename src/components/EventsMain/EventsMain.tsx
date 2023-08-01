634e83081b8d96ee24c809fd8c54bc51293fb01e
Alix2019 <alix1982@yandex.ru>
2023-08-01 21:54:13 +0300

    fix_shadow_border_EventsCardMain_EventsMainPopup

--- src/components/EventsMain/EventsMain.tsx
+++ src/components/EventsMain/EventsMain.tsx
@@ -20,9 +20,14 @@ export const EventsMain: React.FC<Props> = ({events, fetchEvents}) => {
     <div className={styles.events}>
       <h3 className={styles.title}>Предстоящие мероприятия</h3>
       <ul className={styles.list}>
-        {events.length > 0 && events.map((item) =>
-          <EventsMainCard key={item.id} item={item}  fetchEvents={fetchEvents}/>
-        )}
+        {events.length > 0 ?
+          events.map((item) =>
+            <EventsMainCard key={item.id} item={item}  fetchEvents={fetchEvents}/>
+          ) :
+          <li className={styles.pointNoEvents}>
+            <p>Пока нет запланированных мероприятий</p>
+          </li>
+        }
       </ul>
     </div>
   );
