93db19551468c212e2f0fbd16c217bdd18eff34c
Kirill Kurentsov <immortalsteel@gmail.com>
2023-08-03 21:46:32 +0300

    fix: textarea char counter for landing page (#113)
    * fix: textarea char counter for landing page
    
    * fix: change char counter font color
    
    * fix: char counter paddings
    
    * fix: delete link underline for EULA

--- src/shared/ui/Textarea/Textarea.tsx
+++ src/shared/ui/Textarea/Textarea.tsx
@@ -1,6 +1,7 @@
 import { FieldHookConfig, useField } from "formik";
 import styles from "./Textarea.module.scss";
 import { ErrorMessage } from "@/shared/ui/ErrorMessage/ErrorMessage";
+import { useState } from "react";
 
 interface OtherProps {
   label: string;
@@ -9,10 +10,18 @@ interface OtherProps {
 
 export const Textarea = (props: OtherProps & FieldHookConfig<string>) => {
   const [field, meta] = useField(props);
+  const [charCount, setCharCount] = useState(0);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    setCharCount(value.length);
+    field.onChange(e);
+  };
 
   return (
-    <>
+    <div style={{ position: "relative" }}>
       <textarea
+        onInput={handleInputChange}
         placeholder={props.placeholder}
         className={
           meta.touched && meta.error
@@ -21,10 +30,17 @@ export const Textarea = (props: OtherProps & FieldHookConfig<string>) => {
         }
         {...field}
       />
+      <span
+        className={`${styles.charCount} ${
+          meta.touched && meta.error ? styles.charCountErrored : ""
+        }`}
+      >
+        {charCount}/{500}
+      </span>
 
       {meta.touched && meta.error ? (
         <ErrorMessage>{meta.error}</ErrorMessage>
       ) : null}
-    </>
+    </div>
   );
 };
